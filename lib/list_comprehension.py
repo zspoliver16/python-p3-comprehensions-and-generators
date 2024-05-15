df38c397052989c2faadaf63ca748cb337bfe554
zachary oliver <zspoliver@gmail.com>
2024-05-15 11:49:23 -0400

    adds p3 comprehensions lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
